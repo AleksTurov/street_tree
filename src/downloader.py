2b7449b9980962d7103396de24a54d8d9cfb3599
ALEKSEI <119919002+AleksTurov@users.noreply.github.com>
2025-03-27 10:42:22 +0000

    Добавлен модуль для загрузки и обработки моделей, включая функции для загрузки, предсказания и обработки данных, а также обновления .gitignore для исключения новых файлов.

---
+++ src/downloader.py
@@ -0,0 +1,40 @@
+
+import pandas as pd
+from src.config import PATH_MODELS, PATH_PROCESSED
+
+
+def downloader_model(PATH_MODELS):
+    # Пути к моделям и энкодерам
+    model_path = f"{PATH_MODELS}/tabular_model.pth"
+    scaler_path = f"{PATH_MODELS}/scaler.pkl"
+    encoders_path = f"{PATH_MODELS}/label_encoders.pkl"
+
+    target_encoder_path = f"{PATH_MODELS}/label_encoders_target.pkl"
+    target_mapping = joblib.load(target_encoder_path)
+    inverse_target_mapping = {v: k for k, v in target_mapping.items()}
+    logger.info(f"{inverse_target_mapping} - target mapping")
+
+    # Load the checkpoint to get the parameters
+    checkpoint = torch.load(model_path)
+    input_dim = checkpoint['input_dim']
+    hidden_dims = checkpoint['hidden_dims']
+    output_dim = checkpoint['output_dim']
+    target_mapping = checkpoint['target_mapping']
+
+    # Create dummy data with the correct shape
+    dummy_X = pd.DataFrame(np.zeros((3, input_dim)))
+    dummy_y = pd.Series([0, 1, 2]) 
+
+    # Initialize the model using dummy data
+    loaded_model = TabularNN(dummy_X, dummy_y, dummy_X, dummy_y, hidden_dims=hidden_dims)
+    loaded_model.load_state_dict(checkpoint['model_state_dict'])
+    loaded_model.eval()
+
+    logger.info("Model loaded successfully!")
+
+    # Обратное отображение для целевого признака
+    scaler = joblib.load(scaler_path)
+    label_encoders = joblib.load(encoders_path)
+    target_mapping = joblib.load(target_encoder_path)
+    inverse_target_mapping = {v: k for k, v in target_mapping.items()}
+    return loaded_model, inverse_target_mapping, label_encoders, scaler
